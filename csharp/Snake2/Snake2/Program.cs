00526a2513a1503df6957e6f66be7fca79bc6fbd
yuriy <gvoron86@mail.ru>
2018-06-04 15:35:09 +0300

    add gameover

--- csharp/Snake2/Snake2/Program.cs
+++ csharp/Snake2/Snake2/Program.cs
@@ -49,7 +49,27 @@ namespace Snake2
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-            }    
+            }
+            WriteGameOver();
+            Console.ReadLine();
+        }
+
+
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText(String text, int xOffset, int yOffset)
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine(text);
         }
     }
 }
